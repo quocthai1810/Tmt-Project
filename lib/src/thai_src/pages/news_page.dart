146c55f4f4d31194bc58d7cfb846543e119588d1
quocthai1810 <thaiq434@gmail.com>
2025-08-25 08:35:06 +0700

    sua loi vi thg Minh code tren nhanh Thai ma k xoa xong commit luon dcm

--- lib/src/thai_src/pages/news_page.dart
+++ lib/src/thai_src/pages/news_page.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tmt_project/core/widgets/minh/customToast.dart';
-import 'package:tmt_project/core/widgets/tin/custom_button.dart';
-import 'package:tmt_project/core/widgets/tin/custom_progress_indicator.dart';
-import 'package:tmt_project/routers/app_route.dart';
-import 'dart:async';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
@@ -13,62 +8,8 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
-  /// gọi hàm này để set time cho việc chặn đứng mọi hoạt động
-  /// khi sử dụng thì sẽ cho phép người dùng đó đợi ở thời gian được set: delay:
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const Text("Đây là trang home"),
-          OrbitLoadingLogo(
-            // gọi khứa này theo form như này là xài như bth
-            imageAsset: 'assets/img/logo.png',
-            logoSize: 90,
-            dualOpposite: true,
-            orbitRadius: 70,
-            dotSize: 12,
-            trailCount: 10,
-            trailGapDeg: 12,
-            trailOpacityStart: 0.5,
-            trailMinScale: 0.25,
-            width: 100,
-            height: 120,
-            showOrbitRing: true,
-          ),
-          // 🔥 nút này đã đổi onPressed sang _navigateWithLoader
-          CustomButton(
-            text: "Nhấn để test Navigator sang trang test wid",
-            onPressed:
-                () => navigateWithOrbitLoaderNamed(
-                  context,
-                  AppRouteNames.pageTestWid,
-                ),
-            width: 300,
-          ),
-          CustomButton(
-            text: "Thử đi cưng",
-            onPressed: () => debugPrint("Anh vừa bấm đó nghen 😘"),
-            width: 150,
-            height: 25,
-          ),
-          CustomButton(
-            text: "Thử đi cưng",
-            onPressed: () {
-              CustomToast.show(
-                context,
-                message: "nè bé",
-                type: ToastType.success,
-              );
-              debugPrint("Anh vừa bấm đó nghen 😘");
-            },
-            width: 200,
-            height: 50,
-          ),
-        ],
-      ),
-    );
+    return Text("Đây là trang tin tức");
   }
 }
